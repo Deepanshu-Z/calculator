af5b3cbde4e2177c4c2d34a1bc980d5046a25489
Deepanshu <deepanshupokhriyal07@gmail.com>
2024-12-16 23:43:54 +0530

    some changes

--- script.js
+++ script.js
@@ -4,7 +4,33 @@ function getVal(butt){
     displayVal(value);
 }
 
+let lastOperatorPressed = false;
+let val1 = 1, val2, result = 0, counter = 0, numValue = 0;
 function displayVal(value){
     const display = document.querySelector('.showCalc');
-    display.innerHTML += value;
+
+    if (['+', '-', '*', '/', '='].includes(value)) {
+        
+        if (lastOperatorPressed) {
+            // display.innerHTML = 
+        }
+
+        else{
+            display.innerHTML += value;
+            lastOperatorPressed = true;
+            counter = 0;
+            val1 = 1;
+            result = 0;  
+        }
+
+    }
+    
+    else{
+        display.innerHTML += value;
+        let numValue = parseInt(value);
+        val1 = (Math.pow(10, counter));
+        result += val1*numValue;
+        counter++; 
+        console.log(value + " " + result);
+    }
 }
